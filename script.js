b8e09c4b7d6beb3888cbab113f72cb06c18572da
365NICHIKAMIHIKOUKI2024 <saonoi.bnk48thfc@gmail.com>
2024-09-14 08:53:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function tweetMessage() {
-    const message = "365 Group Photo \n#2024_365NichinoKamihikoukiTH ";
+    const message = "365 MV Teaser \n#2024_365NichinoKamihikoukiTH ";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }  
